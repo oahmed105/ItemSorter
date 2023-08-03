080389f6bacdba2789bb349dda99778396874b71
oahmed105 <oahmed10598@gmail.com>
2023-08-03 13:18:08 -0400

    comparators tested

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -10,6 +10,6 @@ import java.util.Comparator;
 public class NameComparator implements Comparator<Item> {
     @Override
     public int compare(Item o1, Item o2) {
-        return (int) (o1.getName() - o2.getName());
+        return o1.getName().compareTo(o2.getName());
     }
 }
